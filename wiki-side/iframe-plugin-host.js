1bd23c9b6b621fc06ddc58b21a7ef8aadd77231d
Brion Vibber <brion@pobox.com>
2016-04-01 17:53:30 +0300

    updates

--- wiki-side/iframe-plugin-host.js
+++ wiki-side/iframe-plugin-host.js
@@ -2,9 +2,10 @@
  * Set up an iframe plugin host within the current element.
  *
  * Events:
- * - 'iframePluginLoaded'
- * - 'iframePluginRequestData'
- * - 'iframePluginRequestAction'
+ * - 'iframePluginLoaded': loaded & ready to go
+ * - 'iframePluginEvent': one-way event notification; details in event.data
+ * - 'iframePluginRequest': plugin-initiated request; details in event.data,
+ *                          respond via Deferred object on event.response
  *
  * @param url {string} target iframe URL
  * @param width {int} CSS pixels width
@@ -25,6 +26,9 @@ $.fn.iframePluginHost = function(url, width, height) {
     }
   } );
 
+  // Counter for events that need unique callbacks
+  this._requestId = 0;
+
   // Create and insert the iframe
   iframe = this._iframePluginFrame = document.createElement( 'iframe' );
   iframe.src = url;
@@ -35,6 +39,11 @@ $.fn.iframePluginHost = function(url, width, height) {
   return this;
 };
 
+/**
+ * Send a one-shot data payload to the plugin.
+ *
+ * @param data {object}
+ */
 $.fn.iframePluginSendData = function( data ) {
   var iframe = this._iframePluginFrame;
   if( iframe ) {
@@ -46,13 +55,39 @@ $.fn.iframePluginSendData = function( data ) {
 
     // We don't force the target origin for now.
     // Safety first! ;)
-    iframe.postMessage( payload, '*');
+    iframe.postMessage( payload, '*' );
   } else {
     // ignore
     console.log('ignoring send data to non-plugin');
   }
 };
 
-$.fn.iframePluginRequestAction = function( action ) {
+/**
+ * Send a data payload to the plugin that expects a response.
+ * Returned data, or failure condition, will be returned through
+ * a promise.
+ *
+ * @param data {object}
+ * @return {jQuery.Promise}
+ */
+$.fn.iframePluginSendRequest = function( data ) {
+  var iframe = this._iframePluginFrame,
+    requestId = ++this._requestId;
+  this.one( 'iframePluginReply' + requestId)
+  if( iframe ) {
 
+    var payload = {
+      format: 'iframePluginHost',
+      action: 'sendRequest',
+      requestId: requestId,
+      data: data
+    };
+
+    // We don't force the target origin for now.
+    // Safety first! ;)
+    iframe.postMessage( payload, '*' );
+  } else {
+    // ignore
+    console.log('ignoring send data to non-plugin');
+  }
 };
