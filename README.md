6e91c1207f0728bb9908ed2597da4e2379f11d69
Brion Vibber <brion@pobox.com>
2016-04-02 12:53:04 +0300

    Update host example jquery plugin & docs
    Use the new example plugin in the example host :D

--- README.md
+++ README.md
@@ -4,3 +4,111 @@ Experimental repo for MediaWiki JavaScript iframe-based plugin system
 Session notes from hackathon: https://etherpad.wikimedia.org/p/WikiHack16-JavaScript
 
 Session task in WMF phab: https://phabricator.wikimedia.org/T131436
+
+Moving parts
+============
+
+Basic parts and terms:
+* host: web application opened directly in the browser
+* plugin: specialized web application that the host opens in an iframe
+* bridge: the communications protocol between host and plugin
+ * message: any datagram sent from one side to the other
+  * payload: the entire JSON object sent via postMessage
+  * data: a JSON object in a payload's 'data' property
+ * event: one-way datagram sent from one side to the other
+ * request: datagram sent across the bridge to initiate a two-way communication
+ * response: datagram sent across the bridge to complete a two-way communication
+
+Low-level bridge protocol
+==============================
+
+The communications bridge between host and plugin uses the [HTML window.postMessage](https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage) interface.
+
+There are two low-level types of messages: one-off "event" messages and two-way "request"/"response" messages.
+
+The same message types are used in both directions; different events and data payloads will be specified on top of this to create an application-specific embedding protocol.
+
+One-way "send" messages have this layout:
+
+```
+  {
+    format: "iframePluginHost",
+    event: <string>,
+    data: { ... }
+  }
+```
+
+These are sent on a "fire and forget" basis; no response is expected.
+
+Two-way "request" messages are composed of a request followed by a response in the other direction. Request messages look like this:
+
+```
+  {
+    format: "iframePluginHost",
+    request: <string>,
+    requestId: <integer>,
+    data: { ... }
+  }
+```
+
+The requestId is selected by the initator of the request, and must be unique within the data stream. This can be easily accomplished by incrementing a counter.
+
+
+A response message in the other direction looks like:
+
+```
+  {
+    format: "iframePluginHost",
+    responseId: <integer>,
+    data: { ... }
+  }
+```
+
+The responseId value is the requestId provided in the request; this allows the other side to associate the reply with the correct handler.
+
+App-level protocol for MediaWiki embedding
+==========================================
+
+For this initial demo, things are very simple. :)
+
+Order of operations:
+
+* host opens iframe with the plugin's target URL
+* plugin loads
+* plugin sends 'loaded' event to host when ready
+* plugin sends 'info' request to host
+ * host responds with wiki's base URL and article title
+
+loaded event
+------------
+
+Sent from plugin to host when ready. Allows host to know when the plugin is ready to accept events/requests.
+
+Data payload: empty/unused
+
+info request
+------------
+
+Request sent from plugin to host to ask for environmental state.
+
+Request data payload: empty/unused (or specify which info we need?)
+
+Response data payload:
+```
+  {
+    url: <string>,
+    title: <string>
+  }
+```
+
+@todo spec this out to be more generally useful
+
+
+Open questions and todos
+========================
+
+* timeouts for request/response?
+* higher-level protocol features
+ * trust/permission requests?
+ * safety & sending of code from host to client?
+  * eg providing standard libraries without having to bundle them into the extensions
--- wiki-side/iframe-plugin-host.js
+++ wiki-side/iframe-plugin-host.js
@@ -1,8 +1,8 @@
 /**
  * Set up an iframe plugin host within the current element.
+ * Implemented as jQuery plugin for convenient event handling.
  *
  * Events:
- * - 'iframePluginLoaded': loaded & ready to go
  * - 'iframePluginEvent': one-way event notification; details in event.data
  * - 'iframePluginRequest': plugin-initiated request; details in event.data,
  *                          respond via Deferred object on event.response
@@ -15,16 +15,47 @@ $.fn.iframePluginHost = function(url, width, height) {
   var iframe;
 
   // @todo how/when do we remove this handler?
-  $( window ).on( 'message', function( event ) {
+  $( window ).on( 'message', ( function( event ) {
     if( event.originalEvent.source === iframe.contentWindow ) {
-      // @fixme handle the events!
-      var data = event.originalEvent.data;
-      console.log( data );
+      var payload = event.originalEvent.data;
+
+      if (typeof payload === 'object' && payload.format === 'iframePluginHost' ) {
+        if ( payload.event ) {
+          this.trigger( 'iframePluginEvent' + payload.event, {
+            data: payload.data
+          } );
+        }
+        if ( payload.request ) {
+          var r = $.Deferred();
+
+          // If the plugin sent us a request, pass that on down to our listeners.
+          // The event.response object will be a jQuery.Deferred that the listener
+          // can resolve, triggering the low-level response message.
+          this.trigger( 'iframePluginRequest' + payload.request, {
+            requestId: payload.requestId,
+            data: payload.data,
+            response: r
+          } );
+          r.promise().done( ( function( responseData ) {
+            this.iframePluginSendPayload( {
+              responseId: payload.requestId
+            } );
+          } ).bind( this ) ).fail( ( function( responseError ) {
+            console.log( 'error condition returned to response. not sure what to do', responseError );
+          } ) );
+        }
+        if ( payload.responseId ) {
+          this.trigger( 'iframePluginReply' + payload.responseId, {
+            data: payload.data
+          } );
+        }
+      } else {
+        console.log( 'Ignoring message from plugin in unexpected format', payload );
+      }
     } else {
-      console.log( event );
-      throw 'Unexpected event source: message not from frame';
+      // ignore other messages, they are not for us
     }
-  } );
+  } ).bind( this ) );
 
   // Counter for events that need unique callbacks
   this._requestId = 0;
@@ -40,26 +71,40 @@ $.fn.iframePluginHost = function(url, width, height) {
 };
 
 /**
- * Send a one-shot data payload to the plugin.
+ * Send a low-level message payload to the plugin.
+ * Meant for internal use.
  *
- * @param data {object}
+ * @access private
+ * @param payload {object}
+ * @return {jQuery.Promise}
  */
-$.fn.iframePluginSendData = function( data ) {
-  var iframe = this._iframePluginFrame;
+$.fn.iframePluginSendPayload = function( payload ) {
+  var iframe = this._iframePluginFrame,
+    r = $.Deferred();
   if( iframe ) {
-    var payload = {
-      format: 'iframePluginHost',
-      action: 'sendData',
-      data: data
-    };
-
     // We don't force the target origin for now.
     // Safety first! ;)
     iframe.postMessage( payload, '*' );
+    r.resolve();
   } else {
     // ignore
-    console.log('ignoring send data to non-plugin');
+    r.reject( 'cannot send event to a non-plugin' );
   }
+  return r.promise();
+};
+
+/**
+ * Send a one-shot data payload to the plugin.
+ *
+ * @param eventName {string} app-specific string key such as "loaded", etc
+ * @param data {object}
+ */
+$.fn.iframePluginSendEvent = function( eventName, data ) {
+  return this.iframePluginSendPayload({
+    format: 'iframePluginHost',
+    event: eventName,
+    data: data
+  });
 };
 
 /**
@@ -67,27 +112,35 @@ $.fn.iframePluginSendData = function( data ) {
  * Returned data, or failure condition, will be returned through
  * a promise.
  *
+ * @param requestName {string} app-specific string key such as "info", "save", etc
  * @param data {object}
  * @return {jQuery.Promise}
  */
-$.fn.iframePluginSendRequest = function( data ) {
+$.fn.iframePluginSendRequest = function( requestName, data ) {
   var iframe = this._iframePluginFrame,
-    requestId = ++this._requestId;
-  this.one( 'iframePluginReply' + requestId)
-  if( iframe ) {
+    r = $.Deferred();
 
-    var payload = {
+  if( iframe ) {
+    var requestId = ++this._requestId;
+
+    this.iframePluginSendPayload( {
       format: 'iframePluginHost',
-      action: 'sendRequest',
+      request: requestName,
       requestId: requestId,
       data: data
-    };
+    } ).fail( r ).done( function() {
+      // Use our custom event forwarding to detect the reply
+      this.one( 'iframePluginReply' + requestId, function( event ) {
+        // @todo confirm format, etc
+        var message = event.data;
+        r.resolve( message.data );
+      } );
+      // @todo handle timeouts/failures
+    } );
 
-    // We don't force the target origin for now.
-    // Safety first! ;)
-    iframe.postMessage( payload, '*' );
   } else {
     // ignore
-    console.log('ignoring send data to non-plugin');
+    r.reject( 'cannot send request to a non-plugin' );
   }
+  return r.promise();
 };
